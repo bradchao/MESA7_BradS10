2b26328c05238a81212f81ff23180c400f9a88d2
iii <iii@userteki-MacBook.local>
2017-06-16 14:35:02 +0800

    v2

---
+++ BradS10/MyClasses.swift
@@ -0,0 +1,37 @@
+//
+//  MyClasses.swift
+//  BradS10
+//
+//  Created by iii on 2017/6/16.
+//  Copyright © 2017年 iii. All rights reserved.
+//
+
+import Foundation
+
+class MyClass01 {
+    var x: Int
+    init(x :Int){self.x = x}
+    subscript(y:Int) -> String {
+        return "\(x) x \(y) = \(x * y)"
+    }
+    subscript(y:Int,z:Int)->Int{
+        return 123
+    }
+    subscript(y:Double) -> Double {
+        get{
+            print("get")
+            return floor(y)
+        }
+        set {
+            print("set")
+            self.x = Int(ceil(newValue))
+        }
+    }
+    
+}
+
+
+
+
+
+
